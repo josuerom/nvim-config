bee9b41f373f46582c99a91075ac682bebd4ecbe
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-04-30 22:54:23 -0500

    Update files from nvim

--- projects/java/Fibonacci.java
+++ projects/java/Fibonacci.java
@@ -1,13 +1,13 @@
 import java.util.Arrays;
 
 /**
-	*@author Josué Romero
+	*@author @{username:Josué Romero}
 	*@param 13/abril/2022 - 21:55 COL
 */
 public class Fibonacci {
 	
 	public static void main(String[] args) {
-		// Mostraré la serie de fibonacci hasta el 1597
+		// Mostraré la serie de fibonacci hasta el #4181
 		int acc[] = new int[20];
 		acc[0] = 0;
 		acc[1] = 1;
@@ -15,6 +15,6 @@ public class Fibonacci {
 		for (int i = 2; i <= 19; i++) {
 			acc[i] = (acc[i-2] + acc[i-1]);
 		}
-		System.out.println("Los primeros 20 números de la sucesión de fibonacci son:\n" + Arrays.toString(acc));
+		System.out.println("Los primeros 20 números de la Sucesión de Fibonacci son:\n" + Arrays.toString(acc));
 	}
-:
+}
