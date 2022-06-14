0aadbbcb22484d6c7eea711636e465ec53c46059
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-06-15 01:37:41 +0200

    Alias cmd

---
+++ Alias.cmd
@@ -0,0 +1,27 @@
+@echo off
+
+cls
+echo Buen dia, HACKERMEN JR3!
+echo ------------------------
+
+DOSKEY jv=java --version
+DOSKEY gv=git --version
+DOSKEY nv=node --version
+DOSKEY pv=python --version
+DOSKEY c=cls
+DOSKEY e=exit
+DOSKEY ..=cd..
+DOSKEY gi=git init
+DOSKEY ga=git add .
+DOSKEY gs=git status
+DOSKEY gp=git push
+DOSKEY gl=git pull
+DOSKEY glg=git log --oneline
+DOSKEY gc=git commit -m "Actualización"
+DOSKEY nvim=nvim .
+DOSKEY n=nvim .
+DOSKEY cdn=cd AppData\Local\nvim\
+DOSKEY htop=taskmgr
+DOSKEY shutdown=shutdown /P
+DOSKEY shutdown=shutdown /r
+DOSKEY java=java %
