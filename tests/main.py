851311e442568c0e668858982ca8c52f7c0d45ec
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-06-22 02:34:55 +0200

    Archivos de prueba nvim

--- tests/main.py
+++ tests/main.py
@@ -1,3 +1,8 @@
-# Sucesión fibonacci en solo dos líneas de código
-N = int(input("Which fibonacci number? "));
-print(round(((5**(0.5)+1)/2)**N/(5**(0.5))));
+# Sucesión de fibonacci en solo 7 líneas de código
+def fibo(n):
+    if n < 2:
+        return n
+    return fibo(n-1) + fibo(n-2)
+
+for i in range(20):
+    print(fibo(i), end=", ")
