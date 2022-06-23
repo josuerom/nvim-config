59975b0c1eeb80e6c1c2139917a4b717fb7d7301
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-06-23 03:33:12 +0200

    Delete main.py

--- tests/main.py
+++
@@ -1,8 +0,0 @@
-# Sucesión de fibonacci en solo 7 líneas de código
-def fibo(n):
-    if n < 2:
-        return n
-    return fibo(n-1) + fibo(n-2)
-
-for i in range(20):
-    print(fibo(i), end=", ")
