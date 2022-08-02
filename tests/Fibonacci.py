9c23a7619ae935f1c1377faec59840efd3556926
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-08-01 21:41:56 -0700

    Update Fibonacci.py

--- tests/Fibonacci.py
+++ tests/Fibonacci.py
@@ -1,11 +1,14 @@
+# @author josuerom
+# @param 01/08/22 COL
+
 def fibo(n):
-    if n < 2:
+   if n < 2:
       return n
-    return fibo(n-1) + fibo(n-2)
+   return fibo(n-1) + fibo(n-2)
 
 print("[", end="")
 for i in range(20):
-    if i < 19:
-      print(fibo(i), end=", ")
-    else:
-      print(fibo(i), end="]")
+   if i < 19:
+       print(fibo(i), end=", ")
+   else:
+       print(fibo(i), end="]")
