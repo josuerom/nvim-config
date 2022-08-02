3c29b5a613a34bc7f2046878f574297180e15c56
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-08-01 21:43:19 -0700

    Ejercicios de prueba

--- tests/Fibonacci.ts
+++ tests/Fibonacci.ts
@@ -1,6 +1,10 @@
+/*
+  *@author josuerom
+  *@param 14/04/22 COL
+*/
 const fibo = [0, 1];
 
 for (let i = 2; i <= 19; i++) {
-	fibo[i] = (fibo[i-2] + fibo[i-1]);
+   fibo[i] = (fibo[i-2] + fibo[i-1]);
 }
 console.log(fibo);
