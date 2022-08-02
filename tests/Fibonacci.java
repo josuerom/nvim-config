746138a59bc1b8b8e28edcbb8cb75f6f7a39d8de
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-08-01 21:39:40 -0700

    Update Fibonacci.java

--- tests/Fibonacci.java
+++ tests/Fibonacci.java
@@ -1,18 +1,18 @@
 import java.util.Arrays;
 
 /**
-	* @author Josué Romero
-	* @param 13/marzo/2022 - 13:55 COL
+  *@author josuerom
+  *@param 13/marzo/2022 - 13:55 COL
 */
 public class Fibonacci {
 	
-	public static void main(String[] args) {
-		int fibo[] = new int[20];
-		fibo[0] = 0; fibo[1] = 1;
+   public static void main(String[] args) {
+      int fibo[] = new int[20];
+      fibo[0] = 0; fibo[1] = 1;
 
-		for (int i = 2; i <= 19; i++) {
-			fibo[i] = (fibo[i-2] + fibo[i-1]);
-		}
-		System.out.println("Fibonacci: " + Arrays.toString(fibo));
-	}
+      for (int i = 2; i <= 19; i++) {
+         fibo[i] = (fibo[i-2] + fibo[i-1]);
+      }
+      System.out.println(Arrays.toString(fibo));
+   }
 }
